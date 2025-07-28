a5c4d4d3aa4dd63f41985cf302edb6e8c063e1a5
unknown <faizalfajri890@gmail.com>
2025-07-28 11:48:56 +0700

    ubah nama folder

--- resources/views/tinker.php
+++ resources/views/tinker.php
@@ -55,9 +55,9 @@ DB::table('undangans')->insert([
 Update
 $u = App\Models\Undangan::where('slug', 'mutmainnah&rahmat')->first();
 $u->ayah_pria = 'Sakkatang';
-$u->ayah_wanita = 'Sani';
+$u->ayah_wanita = 'Amina';
 $u->ibu_wanita = 'Iwan';
-$u->ibu_pria = 'Amina';
+$u->ibu_pria = 'Hapsani';
 $u->save();
 
 $u = App\Models\Undangan::where('slug', 'eva-sadhy')->first();
